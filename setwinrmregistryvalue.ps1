585ff3ed990dc4bcb936705a8c0d32ba3a581d40
j-sullivan0 <72814202+j-sullivan0@users.noreply.github.com>
2020-10-15 10:18:56 -0400

    Add files via upload

---
+++ setwinrmregistryvalue.ps1
@@ -0,0 +1,7 @@
+﻿$regvalue = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service -Name AllowAutoConfig
+
+if($regvalue.AllowAutoConfig -ne 0){
+    Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service -Name AllowAutoConfig -Value 0
+}
+
+
